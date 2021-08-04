e9bdd0689f3497b342c783f6b79d8e2d60741b52
Borik Beglaryan <borik.beglaryan@goya.am>
2021-08-04 19:19:07 +0400

    all

--- main.py
+++ main.py
@@ -5,12 +5,55 @@ import math
 ker_time = 0
 
 
+def go_to(ia):
+    bjij[ia]['distance'] -= bjij[ia]['speed']
+    if bjij[ia]['y'] > 0 and bjij[ia]['y'] < 900:
+        bjij[ia]['y'] -= bjij[ia]['amenamotik_y'] * bjij[ia]['speed']
+    elif bjij[ia]['y'] > 0:
+        bjij[ia]['y'] = 899
+    else:
+        bjij[ia]['y'] = 1
+    if bjij[ia]['x'] > 0 and bjij[ia]['x'] < 900:
+        bjij[ia]['x'] -= bjij[ia]['amenamotik_x'] * bjij[ia]['speed']
+    elif bjij[ia]['x'] > 0:
+        bjij[ia]['x'] = 899
+    else:
+        bjij[ia]['x'] = 1
+
+
+def ker_utel(ui, kl):
+    bjij[ui]['energy'] += ker[kl]['energy']
+    bjij[ui]['amenamotik_x'] = None
+    bjij[ui]['amenamotik_y'] = None
+    bjij[ui]["distance"] = None
+    bjij[ui]['amenamotik_num'] = None
+
+
+def bjij_energy(qwert):
+    bjij[qwert]['energy'] -= 0.1
+    if bjij[qwert]['energy'] >= 30:
+        print(bjij[qwert]['energy'])
+        bjij[qwert]['energy'] /= 2
+        print(bjij[qwert]['energy'])
+        create_new_bjij(bjij[qwert]['x'] + 10, bjij[qwert]['y'] + 10)
+        bjij[qwert]['amenamotik_x'] = None
+        bjij[qwert]['amenamotik_y'] = None
+        bjij[qwert]['distance'] = None
+        bjij[qwert]['amenamotik_num'] = None
+        print(bjij[0]['energy'])
+
+
+def blit_ker():
+    for i in range(len(ker)):
+        win.blit(ker_img, (ker[i]['x'], ker[i]['y']))
+
+
 def create_new_bjij(px, py):
     bjij.append({})
     bjij[-1]['x'] = px
     bjij[-1]['y'] = py
     bjij[-1]['speed'] = random.randint(1, 10)
-    bjij[-1]['energy'] = random.randint(1, 10)
+    bjij[-1]['energy'] = random.randint(3, 4)
     bjij[-1]['amenamotik_x'] = None
     bjij[-1]['amenamotik_y'] = None
     bjij[-1]['distance'] = None
@@ -25,9 +68,11 @@ def generate_ker():
         ker[-1]['y'] = random.randint(50, 700)
         ker[-1]['energy'] = random.randint(2, 8)
         ker_time = time.process_time()
+
+
 pygame.init()
 ker = []
-for i in range(10):
+for i in range(15):
     ker.append({})
     ker[-1]['x'] = random.randint(50, 700)
     ker[-1]['y'] = random.randint(50, 700)
@@ -55,6 +100,12 @@ run = True
 clock = pygame.time.Clock()
 e = 0
 
+
+
+
+
+
+
 while run:
     clock.tick(10)
     generate_ker()
@@ -62,21 +113,16 @@ while run:
         if event.type == pygame.QUIT:
             run = False
     win.blit(background, (0, 0))
-    for i in range(len(ker)):
-        win.blit(ker_img, (ker[i]['x'], ker[i]['y']))
-
+    blit_ker()
     s = True
+
+    if len(bjij) > 1:
+        has = 'o'
+
+
+
     for i in range(len(bjij)):
-        bjij[i]['energy'] -= 0.1
-        if bjij[i]['energy'] >= 30:
-            bjij[i]['energy'] /= 2
-            create_new_bjij(bjij[i]['x']+10, bjij[i]['y']+10)
-        if bjij[i]['energy'] <= 0:
-            s = i
-            continue
-
-
-
+        win.blit(bjij_img, (bjij[i]['x'], bjij[i]['y']))
         h = False
         if bjij[i]['amenamotik_x'] is None:
             distance = 100000
@@ -92,61 +138,36 @@ while run:
                     bjij[i]["distance"] = distance
                     number = d
                     bjij[i]['amenamotik_num'] = number
-            if distance <= 5:
-                h = True
-                bjij[i]['energy'] += ker[d]['energy']
-                bjij[i]['amenamotik_x'] = None
-                bjij[i]['amenamotik_y'] = None
-                bjij[i]["distance"] = None
-                delet = number
-            if bjij[i]['y'] > 0 and bjij[i]['y'] < 900:
-                bjij[i]['y'] -= bjij[i]["amenamotik_y"] * bjij[i]['speed']
-            else:
-                if bjij[i]['y'] > 900:
-                    bjij[i]['y'] = 899
-                else:
-                    bjij[i]['y'] = 1
-            if bjij[i]['x'] > 0 and bjij[i]['x'] < 900:
-                bjij[i]['x'] -= bjij[i]["amenamotik_x"] * bjij[i]['speed']
-            else:
-                if bjij[i]['x'] > 900:
-                    bjij[i]['x'] = 899
-                else:
-                    bjij[i]['x'] = 1
-        else:
-            bjij[i]['distance'] -= bjij[i]['speed']
+        go_to(i)
+        if bjij[i]['distance'] <= 5:
+            h = True
+            bjij[i]['energy'] += ker[bjij[i]['amenamotik_num']]['energy']
 
-            if bjij[i]['y'] > 0 and bjij[i]['y'] < 900:
-                bjij[i]['y'] -= bjij[i]['amenamotik_y'] * bjij[i]['speed']
-            else:
-                if bjij[i]['y'] > 900:
-                    bjij[i]['y'] = 899
-                else:
-                    bjij[i]['y'] = 1
-            if bjij[i]['x'] > 0 and bjij[i]['x'] < 900:
-                bjij[i]['x'] -= bjij[i]['amenamotik_x'] * bjij[i]['speed']
-            else:
-                if bjij[i]['x'] > 900:
-                    bjij[i]['x'] = 899
-                else:
-                    bjij[i]['x'] = 1
-            if bjij[i]['distance'] <= 5:
-                h = True
-                bjij[i]['energy'] += bjij[i]['amenamotik_num']
-                delet = bjij[i]['amenamotik_num']
+
+            for j in range(len(bjij)):
+                if bjij[j]['amenamotik_num'] == bjij[i]['amenamotik_num']:
+                    if j != i:
+                        bjij[-1]['amenamotik_x'] = None
+                        bjij[-1]['amenamotik_y'] = None
+                        bjij[-1]['distance'] = None
+                        bjij[-1]['amenamotik_num'] = None
+
+            delet = bjij[i]['amenamotik_num']
 
 
 
-        win.blit(bjij_img, (bjij[i]['x'], bjij[i]['y']))
+        bjij_energy(i)
+        if bjij[i]['energy'] <= 0:
+            s = i
+
+
+
     if h is True:
         bjij[i]['amenamotik_x'] = None
         bjij[i]['amenamotik_y'] = None
+        bjij[i]['distance'] = None
+        bjij[i]['amenamotik_num'] = None
         del ker[delet]
-
-
-
-
-
 
     if s is not True:
         e += 1
